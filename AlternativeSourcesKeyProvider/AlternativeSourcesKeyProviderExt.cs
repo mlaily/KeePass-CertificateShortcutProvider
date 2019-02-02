1c3e34a728df18922e698368c60e1fd19948bd5d
Melvyn Laïly <melvyn.laily@gmail.com>
2019-02-02 09:30:59 +0100

    Make the sample code work

---
+++ AlternativeSourcesKeyProvider/AlternativeSourcesKeyProviderExt.cs
@@ -0,0 +1,44 @@
+﻿using KeePass.Plugins;
+using KeePassLib.Keys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlternativeSourcesKeyProvider
+{
+    public sealed class AlternativeSourcesKeyProviderExt : Plugin
+    {
+        private IPluginHost _host = null;
+        private SampleKeyProvider _prov = new SampleKeyProvider();
+
+        public override bool Initialize(IPluginHost host)
+        {
+            _host = host;
+
+            _host.KeyProviderPool.Add(_prov);
+            return true;
+        }
+
+        public override void Terminate()
+        {
+            _host.KeyProviderPool.Remove(_prov);
+        }
+    }
+
+    public sealed class SampleKeyProvider : KeyProvider
+    {
+        public override string Name
+        {
+            get { return "Sample Key Provider"; }
+        }
+
+        public override byte[] GetKey(KeyProviderQueryContext ctx)
+        {
+            // Return a sample key. In a real key provider plugin, the key
+            // would be retrieved from smart card, USB device, ...
+            return new byte[] { 2, 3, 5, 7, 11, 13 };
+        }
+    }
+}
--- AlternativeSourcesKeyProvider/Properties/AssemblyInfo.cs
+++ AlternativeSourcesKeyProvider/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AlternativeSourcesKeyProvider")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("test")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AlternativeSourcesKeyProvider")]
+[assembly: AssemblyCompany("test")]
+[assembly: AssemblyProduct("KeePass Plugin")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
