ce06f317e5ad6c630a9dc27424ea3332dc2b916e
Frank Morgner <frankmorgner@gmail.com>
2019-10-02 16:52:14 +0200

    move initialization to the menu

--- CertificateShortcutProvider/CertificateShortcutProviderExt.cs
+++ CertificateShortcutProvider/CertificateShortcutProviderExt.cs
@@ -1,9 +1,11 @@
 ﻿using KeePass.Plugins;
+using KeePassLib.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CertificateShortcutProvider
 {
@@ -23,6 +25,28 @@ namespace CertificateShortcutProvider
         public override void Terminate()
         {
             _host.KeyProviderPool.Remove(_provider);
+        }
+
+        public override ToolStripMenuItem GetMenuItem(PluginMenuType t)
+        {
+            if (t == PluginMenuType.Main)
+            {
+                ToolStripMenuItem tsmi = new ToolStripMenuItem();
+                tsmi.Text = "Initialize Certificate Shortcut Provider...";
+                tsmi.Click += this.onOptionsClicked;
+                return tsmi;
+            }
+            return null;
+        }
+
+        private void onOptionsClicked(object sender, EventArgs e)
+        {
+            var keyFilePath = UrlUtil.StripExtension(_host.Database.IOConnectionInfo.Path) + CertificateShortcutProvider.DefaultKeyExtension;
+
+            using (var form = new KeyCreationForm(keyFilePath))
+            {
+                form.ShowDialog();
+            }
         }
     }
 }
